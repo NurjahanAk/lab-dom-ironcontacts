06241d480d0e25050a25b8f0f415c548aaf5e9f7
Nurjahan <nurjahan_nf.2009@Live.com>
2025-02-26 15:49:13 +0100

    finished lab

--- src/index.js
+++ src/index.js
@@ -37,25 +37,76 @@ tableBody.appendChild(exampleRow);
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
-
-// Your code goes here ...
-
+threeContacts.forEach((element)=>{
+  const row = document.createElement("tr");
+row.innerHTML = `
+  <td>
+    <img src="${element.pictureUrl}" />
+  </td>
+  <td> ${element.name} </td>
+  <td> ${element.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(row)
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
-  
+  const deleteButton =row.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", ()=>{row.remove()});
+
+
+
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
-  
-  
+  const likeButton=row.querySelector(".btn-like");
 
+  likeButton.addEventListener("click",()=>{
+    likeButton.classList.toggle("selected")
+  });
+
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+const addRandomButton = document.querySelector("#btn-add-random");
+addRandomButton.addEventListener("click", () => {
+ 
+    // Get a random contact from the remaining contacts
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    const randomContact = contacts.splice(randomIndex, 1)[0];
+
+   
+  const row = document.createElement("tr");
+row.innerHTML = `
+  <td>
+    <img src="${randomContact.pictureUrl}" />
+  </td>
+  <td> ${randomContact.name} </td>
+  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(row)
+});
